ec2df5b1cbc76de0b752c3485a95fff1fa1e7980
Taras Bulubas <tbulubas@gmail.com>
2021-07-18 14:22:04 +0100

    initial commit generated from start.spring.io

---
+++ src/main/java/com/tboptimus/basesmicroservicethymeleaf/BasesMicroserviceThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.tboptimus.basesmicroservicethymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasesMicroserviceThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasesMicroserviceThymeleafApplication.class, args);
+	}
+
+}
